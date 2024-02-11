65dce1a451b0bda324ab3a01ecea288fe1cc731f
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2024-02-11 22:40:25 +0530

    Update App.js

--- 6. Which path to go?/Code/App.js
+++ 6. Which path to go?/Code/App.js
@@ -1,1 +1,51 @@
+import Header from "./components/Header";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import Body from "./components/Body";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
 
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+    return (
+    <div className="app">
+      <Header/>
+      <Outlet/>
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter}/>)
+
+  
